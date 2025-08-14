304a7fe602192c4a5b377a210fed67cd9ff95137
JyoshnaNayanavari1234 <jyoshnanayanavari102@gmail.com>
2025-08-14 11:07:55 +0530

    Add files via upload

---
+++ NeonNumber.java
@@ -0,0 +1,15 @@
+public class NeonNumber{
+    public static void main(String args[]){
+        int num=9;
+        int square=num*num;
+        int sum=0;
+        while(square>0){
+            sum+=square%10;
+            square/=10;
+        }
+        if(sum==num)
+            System.out.println(num+" is Neon Number");
+        else
+            System.out.println(num+"not Neon Number");
+    }
+}
---
+++ roman.java
@@ -0,0 +1,54 @@
+class roman{
+    static int romantoInt(String s){
+        int Int=0;
+        int num=0;
+        for(int i=s.length()-1;i>=0;i--)
+        {
+            switch(s.charAt(i)){
+                case 'I':{
+                    num=1;
+                    break;
+                }
+                case 'V':{
+                    num=5;
+                    break;
+                }
+                case 'X':{
+                    num=10;
+                    break;
+                }
+                case 'L':{
+                    num=50;
+                    break;
+                }
+                case 'C':{
+                    num=100;
+                    break;
+                }
+                case 'D':{
+                    num=500;
+                    break;
+                }
+                case 'M':{
+                    num=1000;
+                    break;
+                }
+                default:{
+                    System.out.println("invalid RomanNumbers");
+                }
+            }
+            if(num*4<Int){
+                Int-=num;
+            }
+            else
+            {
+                Int+=num;
+            }
+        }
+        return Int;
+    }
+    public static void main(String args[]){
+        int num=romantoInt("C");
+        System.out.println(num);
+    }
+}
